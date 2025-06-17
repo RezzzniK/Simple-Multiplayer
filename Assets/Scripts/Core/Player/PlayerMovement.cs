74109d2a9bdda17d813891795594b8312639b4fb
Alexander Reznik <alexander.reznik@play.works>
2025-06-17 14:51:49 +0300

    PlayerMovement

---
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField] InputReaderSO inputReaderSO;
+    [SerializeField] Transform tracks;
+    [SerializeField] Transform body;
+    [SerializeField] Rigidbody2D rigidbody2D;
+    [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float rotationSpeed = 8f;
+
+    Vector2 perviousMovementInput;
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) return;
+        inputReaderSO.MovingEvent += HandleMove;
+    }
+    void Update()
+    {
+        if (!IsOwner) return;
+        tracks.Rotate(0f, 0f, perviousMovementInput.x * (-rotationSpeed) * Time.deltaTime);//by pressing a/d we are getting +- turning vector
+      
+        body.Rotate(0f, 0f, perviousMovementInput.x *  (-rotationSpeed) * Time.deltaTime);
+    }
+    void FixedUpdate()
+    {
+        if (!IsOwner) return;
+        rigidbody2D.linearVelocity = (Vector2)tracks.transform.up * perviousMovementInput.y * moveSpeed;//because we in fixed upd no need to multiply
+                                                                                                        //by Time.fixedDeltaTime
+    }
+    void HandleMove(Vector2 vector)
+    {
+        Debug.Log("" + vector);
+        perviousMovementInput=vector;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner) return;
+        inputReaderSO.MovingEvent -= HandleMove;
+    }
+}
