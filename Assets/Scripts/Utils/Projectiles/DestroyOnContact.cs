b01ce39923b543238f9b836e15e6177d57fe7ea9
Alexander Reznik <alexander.reznik@play.works>
2025-07-02 19:34:33 +0300

    Firing improvements, muzzle flash

--- Assets/Scripts/Utils/Projectiles/DestroyOnContact.cs
+++ Assets/Scripts/Utils/Projectiles/DestroyOnContact.cs
@@ -5,7 +5,7 @@ public class DestroyOnContact : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-       
+        Debug.Log("" + other.gameObject);
         if (!this.gameObject) return;
         Destroy(this.gameObject);
    }
--- Assets/Scripts/Utils/Projectiles/ProjectileLauncher.cs
+++ Assets/Scripts/Utils/Projectiles/ProjectileLauncher.cs
@@ -13,12 +13,17 @@ public class ProjectileLauncher : NetworkBehaviour//must have to recieve RPC
                                                     //so we can read the position as to where to spawn it in
                                                     //and we can also read it rotation to decide what direction
                                                     //to fire projectile in.
+    [SerializeField] GameObject muzzleFlash;
+    [SerializeField] Collider2D playerCollider;//our player collider to not collide when we firing with ourselvs
     [SerializeField] InputReaderSO inputReaderSO;//need it to hook in to the fire event
     [Header("settings")]
     [Range(10f, 100f)]
     [SerializeField] float projectileSpeed = 10f;
     [Range(1f, 5f)]
-    [SerializeField] float fireCoolDown = 1f;
+    [SerializeField] float fireRate = 1f;
+    [SerializeField] float muzzleFlashDuration=0.5f;
+    float perviousFireTime;
+    float muzzleFlashTimer;
     private bool fire;//will check wether or not we are allowed to fire
 
     public override void OnNetworkSpawn()//subscribing to firing event on network spawn
@@ -33,17 +38,30 @@ public class ProjectileLauncher : NetworkBehaviour//must have to recieve RPC
     }
     void Update()
     {
+        if (muzzleFlashTimer > 0f)
+        {
+            muzzleFlashTimer -= Time.deltaTime;
+            if (muzzleFlashTimer <= 0f) muzzleFlash.SetActive(false);
+        }
+
+
         if (!IsOwner) return;
         if (!fire) return;
+        //checing if we allowed to fire (FIRERATE)
+        if (Time.time/*time of the game*/< 1 / fireRate + perviousFireTime) { return; }
         //here we spawning the dummy projectile for client and for server
-        PrimaryFireServerRPC(projectileSpawnPoint.position,projectileSpawnPoint.up);
-        SpawnClientProjectile(projectileSpawnPoint.position,projectileSpawnPoint.up);
+        PrimaryFireServerRPC(projectileSpawnPoint.position, projectileSpawnPoint.up);
+        SpawnClientProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+        perviousFireTime = Time.time; //potential issue if server is 
+                                     // on for years without restart we will have overflow 
+                                    //max value of the float number
         
        
     }
     private void SpawnClientProjectile(Vector3 spawnPos, Vector3 direction)//private void SpawnClientProjectile(Vector3 spawnPos, Vector3 direction)
     {
-        
+        muzzleFlash.SetActive(true);
+        muzzleFlashTimer = muzzleFlashDuration;//adding timer value(in update we will coundown every tick until<=0 and disable it)
         GameObject projectileInstance = Instantiate(
                                                  clientProjectile,
                                                  spawnPos,
@@ -52,8 +70,17 @@ public class ProjectileLauncher : NetworkBehaviour//must have to recieve RPC
         we will do this in next line*/          Quaternion.identity
                                         );
         projectileInstance.transform.up = direction;//now it getting the turret direction
-        //next is to do: add velocity to rigidbody
+                                                    //next is to do: add velocity to rigidbody
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());//ignoring own collider with own projectile:
 
+        /**ADDING VELOCITY TO PROJECTILE, BY USING RIGIDBODY 2D OF IT
+            also perfroming check if there's rigidbody and will use out for extracting
+        */
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = rb.transform.up*projectileSpeed;
+        }
+        
     }
 
     [ServerRpc]//Http post like header before method,it will take the same parametrs as a SpawnClientProjectile
@@ -65,6 +92,12 @@ public class ProjectileLauncher : NetworkBehaviour//must have to recieve RPC
                                                 Quaternion.identity
                                        );
         projectileInstance.transform.up = direction;//now it getting the turret direction
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());//ignoring own collider with own projectile:
+        
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = rb.transform.up*projectileSpeed;
+        }
         SpawnClientRPC(spawnPos, direction);
        
     }
